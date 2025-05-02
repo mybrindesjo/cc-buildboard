f2ecb7812bec6e78cb7c51c33c3cfcda2c194536
My <my.brindesjo@gmail.com>
2025-05-02 14:19:37 +0200

    Refactor app: remove WelcomePage, update routing to Home and TodoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import WelcomePage from '../component/WelcomePage';
-import TodoList from '../component/TodoList';
-import Navbar from '../component/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+import TodoList from '../component/TodoList'
+import React from 'react'
+import Navbar from '../component/Navbar'
+import Home from '../pages/Home'
 
-const App = () => {
+function App() {
+
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/app" element={<TodoList />} />
-      </Routes>
-    </Router>
-  );
-};
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todos" element={<TodoList />} />
+        </Routes>
+      </Router>
 
-export default App;
+    </>
+  )
+}
+
+export default App
